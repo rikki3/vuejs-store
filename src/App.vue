4db98aab15c99428bd0c821507a8411a2d9a990b
Rickesh Singh <darsanrickesh@gmail.com>
2024-10-23 10:48:37 +0200

    initial cli commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="display: flex; justify-content: space-between; align-content: center;">
+    <img alt="Vue logo" src="./assets/logo.png" class="headerLogo">
+    <div style="display: flex; justify-content: center;">
+    <input type="text" placeholder="Search . . ." class="searchInput"><img alt="Search icon" src="./assets/search-icon.svg" style="max-width: 24px;max-height: 24px;">
+    </div>
+    <HamburgerMenu/>
+  </div>
+  <!-- <router-view/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HamburgerMenu from './components/HamburgerMenu.vue';
+``
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HamburgerMenu,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 60px; */
+  }
+ .headerLogo {
+  max-width: 15%;
+  max-height: 15%;
+ }
+.searchInput {
+    border: none;
+    background-color: transparent;
+    font-size: 18;
+    color: black;
+    max-width: 165px;
+    max-height: 32px
 }
 </style>
---
+++ src/components/HamburgerMenu.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+      <div class="hamburger" @click="toggleMenu">
+        <div class="bar" :class="{ 'open': isOpen }"></div>
+        <div class="bar" :class="{ 'open': isOpen }"></div>
+        <div class="bar" :class="{ 'open': isOpen }"></div>
+      </div>
+      <nav v-if="isOpen" class="menu">
+        <ul>
+          <li @click="navigateTo('home')">Home</li>
+          <li @click="navigateTo('about')">About</li>
+          <li @click="navigateTo('contact')">Contact</li>
+        </ul>
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isOpen: false,
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isOpen = !this.isOpen;
+      },
+      navigateTo(page) {
+        console.log(page)
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .hamburger {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 40px;
+    height: 40px;
+  }
+  
+  .bar {
+    height: 4px;
+    width: 100%;
+    background-color: #333;
+    transition: 0.3s;
+  }
+  
+  .open {
+    background-color: #42b883
+  }
+  
+  .menu {
+    background-color: #f8f9fa;
+    position: absolute;
+    top: 20px;
+    right: 7%;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .menu ul {
+    list-style: none;
+    padding: 2px;
+  }
+  
+  .menu li {
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  
+  .menu li:hover {
+    background-color: #e9ecef;
+  }
+  </style>
+  
