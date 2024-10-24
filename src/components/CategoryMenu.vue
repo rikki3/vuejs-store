679441b53f0a55742df01a2072eb7bd126cc7253
Rickesh Singh <darsanrickesh@gmail.com>
2024-10-24 16:59:10 +0200

    Updating Swiperjs component

--- src/components/CategoryMenu.vue
+++ src/components/CategoryMenu.vue
@@ -1,70 +1,71 @@
 <template>
-    <div>
-        <h1>Hello world!</h1>
-        <span>
-            Welcome to the Vue.js portfolio of<br/>
-            Rickesh Darsan Singh!
-        </span>
-    </div>
-    <div>
-        <swiper
-    :modules="modules"
-    :slides-per-view="1"
-    :space-between="50"
-    navigation
-    :pagination="{ clickable: true }"
-    :scrollbar="{ draggable: true }"
-    :loop="true"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-        <swiper-slide>
-            <img src="../assets/logo.png"/><br/>
-            <h3>
-                Slide 1
-            </h3>
-        </swiper-slide>
-        <swiper-slide>
-            <img src="../assets/logo.png"/><br/>
-            <h3>
-                Slide 2
-            </h3>
-        </swiper-slide>
-        <swiper-slide>
-            <img src="../assets/logo.png"/><br/>
-            <h3>
-                Slide 3
-            </h3>
-        </swiper-slide>
+  <div>
+    <h1>Hello world!</h1>
+  </div>
+  <div>
+    <swiper :modules="modules" :slides-per-view="1" :space-between="50" navigation :pagination="{ clickable: true }"
+      :scrollbar="{ draggable: true }" :loop="true" :centeredSlides="true"
+    :autoplay="{
+      delay: 2500,
+      disableOnInteraction: false,
+    }" @swiper="onSwiper" @slideChange="onSlideChange">
+      <swiper-slide>
+        <img src="../assets/logo.png" /><br />
+        <h3>
+          Slide 1
+        </h3>
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/logo.png" /><br />
+        <h3>
+          Slide 2
+        </h3>
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/logo.png" /><br />
+        <h3>
+          Slide 3
+        </h3>
+      </swiper-slide>
     </swiper>
-    </div>
+  </div>
+  <div>
+    <span>
+      Welcome to the Vue.js portfolio of<br />
+      Rickesh Darsan Singh!
+    </span>
+  </div>
 </template>
 
 <script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
 
-  // Import Swiper styles
-  import 'swiper/css';
+// Import Swiper styles
+import 'swiper/css';
 
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      const onSwiper = (swiper) => {
-        console.log(swiper);
-      };
-      const onSlideChange = () => {
-        console.log('slide change');
-      };
-      return {
-        onSwiper,
-        onSlideChange,
-      };
-    },
-  };
+// import required modules
+import { Autoplay } from 'swiper/modules';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+      modules: [Autoplay]
+    };
+  },
+};
 </script>
 
 <style></style>
